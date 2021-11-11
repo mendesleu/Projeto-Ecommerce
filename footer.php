82aa4a29b08a4cd6a2d40892bcfe86572f67b8bf
mendesleu <leonardo.mendes10@hotmail.com>
2021-11-11 16:32:33 -0300

    recriação da home page com variaveis em ingles e correção de alguns bugs

---
+++ footer.php
@@ -0,0 +1,66 @@
+<footer>
+
+    <div id="footer-container">
+
+        <div   div id="footer-menu">
+            <h1 style="font-size: 20px; margin-bottom: 10px;">Informações:</h1>
+            <label><a href="#" class="footer-links-menu">Contato</a></label>
+            <label><a href="#" class="footer-links-menu">Politicas de Privacidade</a></label>
+            <label><a href="#" class="footer-links-menu">Envio</a></label>
+            <label><a href="#" class="footer-links-menu">Trocas e Devoluções</a></label>
+        </div>
+
+    <div style="width: 300px; display: flex; flex-direction: column;">
+        <h1 style="font-size: 20px; margin-bottom: 10px;">Contato:</h1>
+        <label>Telefone: (00) 0000-0000</label>
+        <label>E-mail: seuemail@servidor.com</label>
+        <label>Rua Mora Naquela casa legal ali Nº 123</label>
+        
+    </div>
+
+    <div style="width: 350px;">
+        <h1 style="font-size: 20px; margin-bottom: 10px;">Sobre-nós:</h1>
+        <p>
+            Aqui vai um pequeno texto falando um pouco sobre a nossa empresa, para você
+            cliente ver que somo legais, um textinho contando nossa pequena historia no
+            comercio, espero que goste.
+        </p>
+    </div>
+
+    <div id="footer-socail">
+
+        <h1 style="font-size: 20px; margin-bottom: 10px;">Social:</h1>
+
+        <div style="display: flex; flex-direction: row;">
+            <div style="width: 30px; margin-right: 5px">
+                <a href="#"><img src="assets/social/face.png" alt="face" style="width: 100%;"></a>
+            </div>
+            <div style="width: 30px; margin-right: 5px">
+                <a href="#"><img src="assets/social/insta.png" alt="insta" style="width: 100%;"></a>
+            </div>
+            <div style="width: 30px; margin-right: 5px">
+                <a href="#"><img src="assets/social/maps.png" alt="maps" style="width: 100%;"></a>
+            </div>
+            <div style="width: 30px; margin-right: 5px">
+                <a href="#"><img src="assets/social/whats.png" alt="whats" style="width: 100%;"></a>
+            </div>
+        </div>
+
+    </div> <!-- Fim Social -->
+
+</div> <!-- Fim Container -->
+
+<div style="margin-top: 30px; margin-bottom: 20px;">
+    <div style="display: flex; flex-direction: row; align-items: center; justify-content: center;">
+        <img src="assets/correios.png" alt="correios" style="width: 100px; margin-right: 30px;">
+        <img src="assets/pagseguro.png" alt="pagseguro" style="width: 120px">
+    </div>
+</div>
+
+<div id="footer-footer">
+    <div id="footer-foot">
+        <label style="color: white; font-size: 8pt;">InfoTech Informática - 34.676.582/0001-42 | Todos os Direitos Reservados</label>
+    </div>
+</div>
+         
+</footer>
---
+++ header.php
@@ -0,0 +1,97 @@
+<header>
+
+    <div id="line-top">
+        <label style="margin-right: 20px;"><a href="#" class="link-top">Login</a></label>
+        <label style="margin-right: 20px;"><a href="#" class="link-top">Cadastre-se</a></label>
+    </div>
+
+    <div id="container-top">
+        <div style="width: 250px;">
+            <a href="index.html">
+                <img src="assets/logo.png" alt="logo" style="width: 100%;">
+             </a>
+        </div>
+
+        <form action="" method="POST" id="search-form" name="search-form">
+            <input type="search" name="search" id="search">
+            <button id="search-button" name="search-button" value="pesquisar">Pesquisar</button>
+        </form>
+
+        <div id="shopping-cart">
+            <div style="width: 60px; display: flex; justify-content: center;">
+                <a href="#" class="shopping-cart-link">
+                    <img src="assets/shopping-cart.png" alt="shopping-cart" style="width: 100%;">
+                </a>
+            </div>
+            <label><a href="#" class="shopping-cart-link">Carrinho</a></label>
+        </div>
+            </div><!-- Fim Linha Centro Topo -->
+
+            <nav>
+                <ul>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul></li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Item</a>
+                        <ul>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+
+    </header>
