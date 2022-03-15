441afeb1b50f8e86149e1f6c60fe3e3076e86ad2
mendesleu <leonardo.mendes10@hotmail.com>
2022-03-15 18:22:25 -0300

    nome da pagina

--- admin/produtos/cadastrar_produto.php
+++ admin/produtos/cadastrar_produto.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Ecommerce</title>
+    <title>Cadastrar Produto - Ecommerce</title>
 
     <link rel="stylesheet" media="all" type="text/css" href="../../css/admin/style.css" />
     <link rel="stylesheet" media="all" type="text/css" href="../../css/admin/produtos.css" />
