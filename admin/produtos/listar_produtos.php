6b8c35b4c3780e1ff9c5eea1f7f29f893e3d6137
mendesleu <leonardo.mendes10@hotmail.com>
2022-03-31 20:49:51 -0300

    Modal filtrar

--- admin/produtos/listar_produtos.php
+++ admin/produtos/listar_produtos.php
@@ -16,6 +16,24 @@ session_start();
     <link rel="stylesheet" media="all" type="text/css" href="../../css/admin/style.css" />
     <link rel="stylesheet" media="all" type="text/css" href="../../css/admin/produtos.css" />
 
+    <script>
+        function openModal() {
+            document.getElementById('modalFiltrar').style.display = 'block';
+        }
+
+        function closeModal() {
+            document.getElementById('modalFiltrar').style.display = 'none';
+        }
+
+        function marcarTodos(){
+            let x = document.getElementById('marcarTodos').checked;
+
+            if(x == true){
+                document.class
+            }
+        }
+    </script>
+
 </head>
 
 <body>
@@ -24,7 +42,7 @@ session_start();
         <?php include "../menu.php" ?>
     </header>
 
-    <main>
+    <main style="padding-bottom: 80px;">
 
         <section id="containerListarProduto">
 
@@ -35,14 +53,14 @@ session_start();
                     <button class="botao">Buscar</button>
                 </form>
 
-                <button class="botao" onclick="">Filtrar</button>
+                <button class="botao" onclick="openModal()">Filtrar</button>
 
             </div>
 
             <div id="containerListar">
 
                 <div class="boxListar first">
-                    <div style="display: flex; align-items:center; margin-right:10px"><input type="checkbox"></div>
+                    <div style="display: flex; align-items:center; margin-right:10px"><input type="checkbox" onclick="marcarTodos()" id="marcarTodos"></div>
                     <div style="width: 300px;"><strong>Produto</strong></div>
                     <div style="width: 100px; text-align: center;"><strong>Categoria</strong></div>
                     <div style="width: 100px; text-align: center;"><strong>Preço</strong></div>
@@ -54,16 +72,16 @@ session_start();
 
                 $pg = isset($_GET['pg']) ? $_GET['pg'] : "1";
                 $search = isset($_POST['search']) ? $_POST['search'] : "all";
-                
+
                 // Quantidade maxima a exibir por pagina
-                $maximo = 5;
+                $maximo = 10;
 
                 // Calcula a página de qual valor será exibido
                 $inicio = ($maximo * $pg) - $maximo;
 
-                if($search == 'all'){
+                if ($search == 'all') {
                     $select_type = "SELECT * FROM produtos LIMIT $inicio, $maximo";
-                }else{
+                } else {
                     $select_type = "SELECT * FROM produtos WHERE sku LIKE '%$search%' OR nome LIKE '%$search%' OR ean LIKE '%$search%'";
                 }
 
@@ -75,7 +93,7 @@ session_start();
                 ?>
 
                         <div class="boxListar second">
-                            <div style="display: flex; align-items:center; margin-right:10px"><input type="checkbox"></div>
+                            <div style="display: flex; align-items:center; margin-right:10px"><input type="checkbox" id="marcarId<?= $listar['id_produto']?>"></div>
                             <div style="width: 300px;" class="boxNomeProduto"><?= $listar['nome'] ?></div>
                             <div style="width: 100px; text-align: center; text-transform: capitalize;"><?= $listar['categoria'] ?></div>
                             <div style="width: 100px; text-align: center;">R$ <?= number_format($listar['venda'], 2, ",", ".") ?></div>
@@ -120,7 +138,8 @@ session_start();
                     ?>
 
                         <a href="?pg=<?= $anterior ?>">
-                            <div class="boxPaginacao"><strong><</strong>
+                            <div class="boxPaginacao"><strong>
+                                    << /strong>
                             </div>
                         </a>
 
@@ -141,16 +160,16 @@ session_start();
                                 <div class="boxPaginacao"><strong><?= $i ?></strong></div>
                             </a>
 
-                    <?php
+                        <?php
                         }
                     }
 
-                    if($totalPG > 1){
-                    ?>
+                    if ($totalPG > 1) {
+                        ?>
 
-                    <a href="?pg=<?= $totalPG ?>">
-                        <div class="boxPaginacao"><strong><?= $totalPG ?></strong></div>
-                    </a>
+                        <a href="?pg=<?= $totalPG ?>">
+                            <div class="boxPaginacao"><strong><?= $totalPG ?></strong></div>
+                        </a>
 
                     <?php
                     }
@@ -173,6 +192,68 @@ session_start();
         </section>
 
     </main>
+
+    <div class="containerModal" id="modalFiltrar" style="display: none;">
+        <div class="boxModal">
+            <div class="boxConteudoModal">
+
+                <div class="tituloBox tituloFiltrar">
+                    <h4>Filtros</h4>
+                    <input type="button" value="X" id="botaoFecharModal" onclick="closeModal()">
+                </div>
+                <!-- Titulo -->
+
+                <form action="" id="formFiltrar">
+
+                    <div style="display: flex;">
+
+                        <div style="display: flex; flex-direction: column;">
+
+                            <label class="titulo">Ordenar por</label>
+                            <select class="selectFiltrar" name="ordem">
+                                <option value="alfabetica">Ordem alfabetica</option>
+                            </select>
+
+                            <label class="titulo">Status do produto</label>
+                            <select class="selectFiltrar" name="status">
+                                <option>Todos</option>
+                                <option>Ativos</option>
+                                <option>Inativos</option>
+                            </select>
+
+                        </div>
+
+                        <div style="display: flex; flex-direction: column; margin-left: 20px;">
+
+                            <label class="titulo">Produto em destaque</label>
+                            <select class="selectFiltrar" name="destaque">
+                                <option>Selecionar</option>
+                                <option>Sim</option>
+                                <option>Não</option>
+                            </select>
+
+                            <label class="titulo">Produto em sem estoque</label>
+                            <select class="selectFiltrar" name="estoque">
+                                <option>Selecionar</option>
+                                <option>Sim</option>
+                                <option>Não</option>
+                            </select>
+
+                        </div>
+
+                    </div>
+
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <a href="listar_produtos.php" id="botaoLimparFiltro">X Limpar Filtros</a>
+
+                        <button type="submit" class="botao">Filtrar</button>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
 
 </body>
 
